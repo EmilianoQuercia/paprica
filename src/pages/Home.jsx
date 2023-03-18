7f55b6b4b6f987be0d1558409474251b060c28f6
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-03-18 00:43:55 -0300

    Se realizo y termino, nav, footer y portada del home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Portada from '../components/Portada/Portada';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Portada/>
+            <h1>Remplazar por componente carrucel 1</h1>
+            <h1>Remplazar por componente carrucel 2</h1>
+            <h1>Remplazar por componente carrucel 3</h1>
+        </div>
+    );
+}
+
+export default Home;

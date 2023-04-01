40e34ed75a17ba3c490aad8ba19733b628b01d54
juan aguilera <juampiag3@gmail.com>
2023-04-01 19:06:34 -0300

    Retoques finales

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,57 +12,59 @@ const Footer = () => {
           <img src="./assets/img/Logo1.png" alt="" />
           <h5>Las únicas papas de Pereira</h5>
         </div>
-        <div className="infoFooter">
-          <div>
-            <div className="infoTitulos">
-              <GoCalendar />
-              <h4>Dias</h4>
+        <div className="containerInfoFooter">
+          <div className="infoFooter">
+            <div>
+              <div className="infoTitulos">
+                <GoCalendar />
+                <h4>Dias</h4>
+              </div>
+              <p>Martes a Viernes</p>
+              <p>Sábados y Domingos</p>
             </div>
-            <p>Martes a Viernes</p>
-            <p>Sábados y Domingos</p>
-          </div>
-          <div>
-            <div className="infoTitulos">
-              <HiOutlineClock />
-              <h4>Horarios</h4>
+            <div>
+              <div className="infoTitulos">
+                <HiOutlineClock />
+                <h4>Horarios</h4>
+              </div>
+              <p>17:00 a 22:00hs</p>
+              <p>12:00 a 22:00hs</p>
             </div>
-            <p>17:00 a 22:00hs</p>
-            <p>12:00 a 22:00hs</p>
-          </div>
-          <div>
-            <div className="infoTitulos">
-              <MdOutlinePlace />
-              <h4>Ubicación</h4>
+            <div>
+              <div className="infoTitulos">
+                <MdOutlinePlace />
+                <h4>Ubicación</h4>
+              </div>
+              <p>Cra. 37 #30-51, Pereira,</p>
+              <p>Risaralda, Colombia</p>
             </div>
-            <p>Cra. 37 #30-51, Pereira,</p>
-            <p>Risaralda, Colombia</p>
           </div>
-        </div>
-        <div>
-          <a
-            href="https://wa.me/573052975504?text=Hola+Paprika%21+Quisiera+hacer+un+pedido"
-            target={"_blank"}
-            className="infoRedes"
-          >
-            <img src="./assets/img/logoWhatsapp.png" alt="" />
-            <p>+57 305 2975504</p>
-          </a>
-          <a
-            href="https://www.facebook.com/carlo.duarte.37201"
-            target={"_blank"}
-            className="infoRedes"
-          >
-            <img src="./assets/img/logoFacebook.png" alt="" />
-            <p>Paprika Papas Belgas</p>
-          </a>
-          <a
-            href="https://www.instagram.com/paprikapapasbelgas/"
-            target={"_blank"}
-            className="infoRedes"
-          >
-            <img src="./assets/img/logoInsta.png" alt="" />
-            <p>@paprikapapasbelgas</p>
-          </a>
+          <div className="redesFooter">
+            <a
+              href="https://wa.me/573052975504?text=Hola+Paprika%21+Quisiera+hacer+un+pedido"
+              target={"_blank"}
+              className="infoRedes"
+            >
+              <img src="./assets/img/logoWhatsapp.png" alt="" />
+              <p>+57 305 2975504</p>
+            </a>
+            <a
+              href="https://www.facebook.com/carlo.duarte.37201"
+              target={"_blank"}
+              className="infoRedes"
+            >
+              <img src="./assets/img/logoFacebook.png" alt="" />
+              <p>Paprika Papas Belgas</p>
+            </a>
+            <a
+              href="https://www.instagram.com/paprikapapasbelgas/"
+              target={"_blank"}
+              className="infoRedes"
+            >
+              <img src="./assets/img/logoInsta.png" alt="" />
+              <p>@paprikapapasbelgas</p>
+            </a>
+          </div>
         </div>
       </div>
       <div>

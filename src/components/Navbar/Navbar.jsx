a956d1ddec05f89f6344cd4c3d74069ea6df875e
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-04-01 13:13:03 -0300

    acomode medidas de modal para dispositivos moviles mas chicos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -202,11 +202,7 @@ const Navbar = () => {
                     </p>
                     <p>De martes a viernes de 17:00 a 22:00 hs</p>
                     <p>Sábados y Domingos de 12:00 a 22:00 hs</p>
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d993.9675969188418!2d-75.71064874160969!3d4.7922845000728165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38877d26d6f3bf%3A0x6ed53af226731f02!2sVilla%20Verde%20Plaza!5e0!3m2!1ses!2sar!4v1679170834745!5m2!1ses!2sar"
-                      width="100%"
-                      height="220"
-                    ></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d993.9675969188418!2d-75.71064874160969!3d4.7922845000728165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38877d26d6f3bf%3A0x6ed53af226731f02!2sVilla%20Verde%20Plaza!5e0!3m2!1ses!2sar!4v1679170834745!5m2!1ses!2sar"></iframe>
                   </div>
                 </PapasModal>
               )}
